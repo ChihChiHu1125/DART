7a53cb4d1df20910e5c9cab35d0a9fac25b63d29
chihchihu1125 <jerryhu001155@gmail.com>
2023-06-08 13:27:16 -0600

    add generate_dir.sh

---
+++ models/bgrid_solo/work/generate_dir.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# a shell script that generates the directory that is necessary for DART_cylcing
+
+mkdir state_output
+mkdir obs_space_diag
+mkdir log_files
+
+cp filter_prior_0_0.nc ./state_output
+cp merge_netcdf_files.sh ./state_output
+cp ./matlab_diag_scripts/state_diag_noplot.m ./state_output
+cp ./matlab_diag_scripts/cycling_read_obs_seq_files.m ./obs_space_diag
+cp ./matlab_diag_scripts/obs_diag_state_RMSE_spread_at_obs_loc_noplot.m ./obs_space_diag
