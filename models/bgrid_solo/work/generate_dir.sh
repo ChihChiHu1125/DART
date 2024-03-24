c57601256bbf39f97a8c35b6a565801d7341c770
chihchihu1125 <jerryhu001155@gmail.com>
2024-03-23 18:34:27 -0600

    fix a bug in generate_dir.sh

--- models/bgrid_solo/work/generate_dir.sh
+++ models/bgrid_solo/work/generate_dir.sh
@@ -7,7 +7,7 @@ mkdir obs_space_diag
 mkdir log_files
 mkdir temp_for_input_nml
 
-cp filter_prior_0_0.nc ./state_output
+cp filter_prior_20_0.nc ./state_output
 cp merge_netcdf_files.sh ./state_output
 cp ./matlab_diag_scripts/state_diag_noplot.m ./state_output
 cp ./matlab_diag_scripts/cycling_read_obs_seq_files.m ./obs_space_diag
